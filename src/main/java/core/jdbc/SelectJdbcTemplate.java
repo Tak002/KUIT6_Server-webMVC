78c50d82cb46038980913ca0ac79b17d0c53058a
andyn <andycho1120@naver.com>
2025-10-29 17:23:56 +0900

    find 수정중

--- src/main/java/core/jdbc/SelectJdbcTemplate.java
+++ src/main/java/core/jdbc/SelectJdbcTemplate.java
@@ -9,9 +9,21 @@ public class SelectJdbcTemplate<T> {
     private final JdbcTemplate<T> jdbcTemplate = new JdbcTemplate<>();
 
 
-    public List<T> findAll(String userTableName, List<String> columns, Class<T> returnClassType) throws SQLException {
-        String sql = "SELECT * FROM "+ userTableName;
+    public List<T> findAll(String tableName, List<String> columns, Class<T> returnClassType) throws SQLException {
+        String sql = "SELECT * FROM "+ tableName;
         return jdbcTemplate.query(sql, generateRowMapper(columns,returnClassType));
+    }
+    // todo colums, conditions 의 갯수에 따른 sql문 동적 생성
+    public T find(String tableName, List<String> conditionCol, List<String> conditionVal, List<String> columns, Class<T> returnClassType) throws SQLException {
+        String sql = "SELECT * FROM "+ tableName + " WHERE "+ "? = ?,"* conditionCol.size();
+        // column 명은 ?로 주입이 안된다고 함....
+        PreparedStatementSetter preparedStatementSetter = preparedStatement -> {
+            for(int i = 0; i < conditionCol.size(); i+=2) {
+                preparedStatement.setString(i + 1, conditionCol.get(i));
+                preparedStatement.setString(i + 2, conditionVal.get(i));
+            }
+        };
+        return jdbcTemplate.queryForObject(sql,preparedStatementSetter, generateRowMapper(columns,returnClassType));
     }
 
 
--- src/main/java/core/jdbc/UpdateJdbcTemplate.java
+++ src/main/java/core/jdbc/UpdateJdbcTemplate.java
@@ -6,12 +6,13 @@ import java.util.List;
 public class UpdateJdbcTemplate<T> {
     private final JdbcTemplate<T> jdbcTemplate = new JdbcTemplate<>();
 
-    public void update(String tableName, List<String> columns) throws SQLException {
-        String sql = "UPDATE ? SET password = ?, name = ?, email = ? WHERE userId = ?";
+    public void update(String tableName, List<String> columns, List<String> datas) throws SQLException {
+        // todo colums, conditions 의 갯수에 따른 sql문 동적 생성
+        String sql = "UPDATE ? SET ? = ?, ? = ?, ? = ? WHERE ? = ?";
         PreparedStatementSetter preparedStatementSetter = preparedStatement -> {
             preparedStatement.setString(1, tableName);
-            for(int i = 0; i < columns.size(); i++) {
-                preparedStatement.setString(i + 2, columns.get(i));
+            for(int i = 0; i < datas.size(); i+=2) {
+                preparedStatement.setString(i + 2, datas.get(i));
             }
         };
         jdbcTemplate.update(sql, preparedStatementSetter);
